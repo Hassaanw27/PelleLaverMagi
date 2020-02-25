242de95429c982944feb15349053e7d033eac1c7
Hassaanw27 <hassaanw27@gmail.com>
2020-02-25 22:00:24 +0100

    Changed logo, video design, iframe doesnt work due to "samesite" problem

--- main.js
+++ main.js
@@ -8,3 +8,26 @@ $(document).ready(function(){
       });
     });
   });
+
+
+
+//var fixmeTop = $('.scroller').offset().top;       // get initial position of the element
+
+//$(window).scroll(function() {                  // assign scroll event listener
+
+    //var currentScroll = $(window).scrollTop(); // get current position
+
+    //if (currentScroll >= fixmeTop) {           // apply position: fixed if you
+      //  $('.scroller').css({                      // scroll to that element or below it
+            position: 'fixed',
+            top: '0',
+            right: '0',
+
+        });
+    //} else {                                   // apply position: static
+        //$('.scroller').css({                      // if you scroll above it
+            position: 'static'
+        });
+    }
+
+});
