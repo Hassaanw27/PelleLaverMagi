e57fb54a0c08aa557d6852df23d79a1bb246a9eb
Hassaanw27 <hassaanw27@gmail.com>
2020-02-26 12:36:52 +0100

    Added Nav

--- main.js
+++ main.js
@@ -1,33 +1,21 @@
-$(document).ready(function(){
-    $("button").click(function(){
-      $("iframe").animate({
-        right: '250px',
-        opacity: '1',
-        height: '800px',
-        width: '300px'
-      });
+$(document).ready(function() {
+  $("button").click(function() {
+    $("iframe").animate({
+      right: '250px',
+      opacity: '1',
+      height: '800px',
+      width: '300px'
     });
   });
+});
 
-
-
-//var fixmeTop = $('.scroller').offset().top;       // get initial position of the element
-
-//$(window).scroll(function() {                  // assign scroll event listener
-
-    //var currentScroll = $(window).scrollTop(); // get current position
-
-    //if (currentScroll >= fixmeTop) {           // apply position: fixed if you
-      //  $('.scroller').css({                      // scroll to that element or below it
-            position: 'fixed',
-            top: '0',
-            right: '0',
-
-        });
-    //} else {                                   // apply position: static
-        //$('.scroller').css({                      // if you scroll above it
-            position: 'static'
-        });
+$(document).ready(function() {
+  $(window).bind('scroll', function() {
+    var distance = 500;
+    if ($(window).scrollTop() > distance) {
+      $('nav').addClass('scrolled');
+    } else {
+      $('nav').removeClass('scrolled');
     }
-
+  });
 });
