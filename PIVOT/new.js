52937e1678879fdb7105fb21170abce9e6e3c11d
Hassaanw27 <hassaanw27@gmail.com>
2020-03-02 16:26:20 +0100

    Slideshow test

--- PIVOT/new.js
+++ PIVOT/new.js
@@ -18,32 +18,13 @@ function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
 
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+$('#lightSlider').lightSlider({
+    gallery: true,
+    item: 1,
+    loop: true,
+    slideMargin: 0,
+    thumbItem: 9
+});
 
 //$(document).ready(function() {
   //$(window).bind('scroll', function() {
