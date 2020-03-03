2fe7bac5ffb438428271886b77d241499d8ee989
Hassaanw27 <hassaanw27@gmail.com>
2020-03-03 17:23:47 +0100

    samlet

--- PIVOT/new.js
+++ PIVOT/new.js
@@ -1,5 +1,4 @@
-$(document).ready(function() {
-  $("button").click(function() {
+$(".scroller").click(function() {
     $('.eksil').animate({
       right: '1px',
       top: '100px',
@@ -8,7 +7,7 @@ $(document).ready(function() {
       width: '290px'
     });
   });
-});
+
 
 function openNav() {
   document.getElementById("myNav").style.width = "100%";
@@ -17,6 +16,20 @@ function openNav() {
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
+
+
+  if ($("video").prop('muted', true)){
+    $("#mute").css("background-image","url(http://image.flaticon.com/icons/svg/10/10776.svg)");
+  }
+
+$("#mute").click( function (){
+  if( $("video").prop('muted') ) {
+    $("video").prop('muted', false);
+    $("#mute").css("background-image","url(http://image.flaticon.com/icons/svg/10/10430.svg)");
+  } else {
+    $("video").prop('muted', true);
+    $("#mute").css("background-image","url(http://image.flaticon.com/icons/svg/10/10776.svg)");
+  }
 
 let slides = document.getElementById('slideshow');
 let imgs = slides.getElementsByClassName('slides');
